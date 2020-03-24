c57bb01ccf02b198847a7db21fd9ff37a9618f0c
fpoljcic <fpoljcic1@etf.unsa.ba>
2020-03-24 22:27:43 +0100

    Project & README.md initialization

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>User Management Web App</h1>
+    </div>
+  );
+}
+
+export default App;
