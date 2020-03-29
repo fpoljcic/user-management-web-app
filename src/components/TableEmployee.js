80618a64358110040eece015973391c61e3775ad
lkafedzic1 <lkafedzic1@etf.unsa.ba>
2020-03-30 00:29:19 +0200

    Implementirati mogućnost pretrage (filtriranja) postojećih uposlenika po svakom atributu uposlenika

--- src/components/TableEmployee.js
+++ src/components/TableEmployee.js
@@ -57,7 +57,75 @@ class TableEmployee extends React.Component {
     });
   };
 
-  
+  getColumnSearchProps = dataIndex => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={node => {
+            this.searchInput = node;
+          }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+
+          onPressEnter={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Button
+          type="primary"
+          onClick={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+          icon={<SearchOutlined />}
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          Search
+        </Button>
+        <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+          Reset
+        </Button>
+      </div>
+    ),
+    filterIcon: filtered => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes(value.toLowerCase()),
+    onFilterDropdownVisibleChange: visible => {
+      if (visible) {
+        setTimeout(() => this.searchInput.select());
+      }
+    },
+    render: text =>
+      this.state.searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          searchWords={[this.state.searchText]}
+          autoEscape
+          textToHighlight={text.toString()}
+        />
+      ) : (
+          text
+        ),      
+   
+  });
+
+  handleSearch = (selectedKeys, confirm, dataIndex) => {
+
+    
+    console.log(this.state);
+    confirm();
+    this.setState({
+      searchText: selectedKeys[0],
+      searchedColumn: dataIndex,
+    });
+  };
+
+  handleReset = clearFilters => {
+    clearFilters();
+    this.setState({ searchText: '' });
+  };
+
 
   render() {
     
@@ -71,69 +139,72 @@ class TableEmployee extends React.Component {
         dataIndex: 'userId',
         key: 'userId',
         filteredValue: filteredInfo.userId || null,
+       
         ellipsis: true,
-     
+        ...this.getColumnSearchProps('userId'),
       },
       {
         title: 'Name',
         dataIndex: 'name',
         key: 'name',
         filteredValue: filteredInfo.name || null,
-
-        ellipsis: true,
     
+        ellipsis: true,
+        ...this.getColumnSearchProps('name'),
       },
       {
         title: 'Surname',
         dataIndex: 'surname',
         key: 'surname',
         filteredValue: filteredInfo.surname || null,
-    
+        
         ellipsis: true,
- 
+        ...this.getColumnSearchProps('surname'),
       },
       {
         title: 'Email',
         key: 'email',
         dataIndex: 'email',
         filteredValue: filteredInfo.email || null,
-
+      
         ellipsis: true,
-       
+        ...this.getColumnSearchProps('email'),
       },
       {
         title: 'Address',
         key: 'address',
         dataIndex: 'address',
         filteredValue: filteredInfo.address || null,
-      
+    
         ellipsis: true,
-      
+        ...this.getColumnSearchProps('address'),
       },
       {
         title: 'Phone number',
         dataIndex: 'phoneNumber',
         key: 'phoneNumber',
-
+        filteredValue: filteredInfo.phoneNumber || null,
+      
         ellipsis: true,
-   
+        ...this.getColumnSearchProps('phoneNumber'),
       },
       {
         title: 'Country',
         dataIndex: 'country',
         key: 'country',
         filteredValue: filteredInfo.country || null,
-              ellipsis: true,
-   
+       
+        ellipsis: true,
+        ...this.getColumnSearchProps('country'),
       },
       {
         title: 'City',
         dataIndex: 'city',
         key: 'city',
         filteredValue: filteredInfo.city || null,
-     
+       
         ellipsis: true,
-
+        ...this.getColumnSearchProps('city'),
       },
       {
         title: 'Edit',
