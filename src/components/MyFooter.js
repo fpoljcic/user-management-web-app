573c60d0fb3ff426e95549b303bb1717830b2f7b
fpoljcic <fpoljcic1@etf.unsa.ba>
2020-04-02 15:28:27 +0200

    Dodata zastita kada je user null

--- src/components/MyFooter.js
+++ src/components/MyFooter.js
@@ -11,9 +11,11 @@ function MyFooter(props) {
         <Footer className="footerStyle">
             <a target="_blank" href="https://github.com/fpoljcic/user-management-web-app">About</a> |
             <a target="_blank" href="mailto:fpoljcic1@etf.unsa.ba"> Contact us</a>
-            <div style={{ marginTop: '-20px', textAlign: 'right' }}>
-                Last login at {user.loginDate + " " + user.loginTime}
-            </div>
+            {user != null ?
+                (<div style={{ marginTop: '-20px', textAlign: 'right' }}>
+                    Last login at {user.loginDate + " " + user.loginTime}
+                </div>) :
+                null}
         </Footer>
     );
 }
