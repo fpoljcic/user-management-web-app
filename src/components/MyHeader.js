e0fb885cf03faecb882e07699b2b03a4e623e665
fpoljcic <fpoljcic1@etf.unsa.ba>
2020-05-04 04:35:00 +0200

    Implementirano primanje notifikacija

--- src/components/MyHeader.js
+++ src/components/MyHeader.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Layout, Menu, Dropdown } from 'antd';
+import React, { useState } from 'react';
+import { Layout, Menu } from 'antd';
 import { Link } from "react-router-dom";
 import { removeUserSession } from '../utilities/Common';
 import Img from 'react-image'
@@ -9,28 +9,13 @@ const { Header } = Layout;
 
 
 function MyHeader(props) {
-
-   
-     var   showNoti = false;
-        
-    
-    function renderNotifications(){
-       
-            
-        return showNoti ? <Notifications />: null;
-    }
-    function toggleNoti(){
-        showNoti = !showNoti; 
-        renderNotifications();       
-    }
+    const [selected, setSelected] = useState("1");
 
     // odjava korisnika
     const handleLogout = () => {
         removeUserSession();
         props.history.push('/');
     }
-
-
 
     return (
         <Header>
@@ -39,50 +24,48 @@ function MyHeader(props) {
                 theme="dark"
                 mode="horizontal"
                 defaultSelectedKeys={[props.page]}
+                selectedKeys={[selected]}
             >
-                <Menu.Item key="1">
+                <Menu.Item key="1" onClick={() => setSelected("1")}>
                     Home
                     <Link to="/dashboard/home" />
                 </Menu.Item>
 
-                <Menu.Item key="2">
+                <Menu.Item key="2" onClick={() => setSelected("2")}>
                     Managers
                     <Link to="/dashboard/managers" />
                 </Menu.Item>
 
-                <Menu.Item key="3">
+                <Menu.Item key="3" onClick={() => setSelected("3")}>
                     Cashiers
                     <Link to="/dashboard/cashiers" />
                 </Menu.Item>
 
-                <Menu.Item key="4">
+                <Menu.Item key="4" onClick={() => setSelected("4")}>
                     Add employees
                     <Link to="/dashboard/add_employee" />
                 </Menu.Item>
 
-                <Menu.Item key="5">
+                <Menu.Item key="5" onClick={() => setSelected("5")}>
                     Manage employees
                     <Link to="/dashboard/manage_employees" />
                 </Menu.Item>
 
-                <Menu.Item key="6" >
+                <Menu.Item key="6" onClick={() => setSelected("6")}>
                     User log
                     <Link to="/dashboard/user_log" />
                 </Menu.Item>
-
-                
 
                 <Menu.Item key="7" style={{ float: "right" }} onClick={handleLogout}>
                     <Img src={require('../img/logout.png')} width='20px' height='20px' />
                     {" "}Log out
                 </Menu.Item>
 
-
-                <Menu.Item key = "8" style={{ float: "right" , zIndex: 1000}} >   
-                    <Notifications />
+                <Menu.Item disabled key="8" style={{ float: "right", zIndex: 1000 }} >
+                    <Notifications setSelected={setSelected} />
                 </Menu.Item>
             </Menu>
-            
+
         </Header>
     );
 }
--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,74 +1,106 @@
-import React, { useState, useEffect } from 'react';
-import Img from 'react-image';
-
-import * as SockJS from 'sockjs-client';
-import * as Stomp from 'stompjs';
-
-
+import React, { useEffect, useState } from 'react';
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs';
+import { Menu, Dropdown } from 'antd';
+import { Link } from 'react-router-dom';
 
 let socket, stompClient;
 
-class Notifications extends React.Component{
+const soundUrl = `https://ia800203.us.archive.org/14/items/slack_sfx/confirm_delivery.mp3`;
+const notificationSound = new Audio(soundUrl);
 
-    state = {
-        allNotifications: [],
-        visibility: true
-    }
-    constructor(props){
-        super(props);
-    }
+const Notifications = (props) => {
+    const [count, setCount] = useState(0);
+    const [notifications, setNotifications] = useState([]);
+    const [sel, setSel] = useState([]);
 
-
-    async componentWillMount(){
-        
-        socket = new SockJS('http://log-server-si.herokuapp.com/ws');
+    useEffect(() => {
+        socket = new SockJS('https://log-server-si.herokuapp.com/ws');
         stompClient = Stomp.over(socket);
-    
+
         stompClient.connect({}, () => {
-        if(stompClient.connected){
-            
-            stompClient.subscribe('/topic/warehouse', (msg) => {
-                let data = JSON.parse(msg.body);
-                console.log(data);
-    
-                let allNoti = this.state.allNotifications;
-                allNoti.push(data);
-                this.setState({allNotifications : allNoti});
-                
-              });
-        }
-          
-        }, err => console.error(err));
-    
+            if (stompClient.connected) {
+                stompClient.subscribe('/topic/user_management', (notif) => {
+                    let notification = JSON.parse(notif.body);
+                    notifications.unshift(notification);
+                    sel.unshift(1);
+                    setSel(sel);
+                    setNotifications(notifications);
+                    setCount(count + 1);
+                    notify();
+                });
+            }
+        }, (err) => {
+            console.log('error pri konekciji ' + err);
+        });
+
         return () => stompClient.disconnect();
+    }, [count, notifications, sel]);
+
+    const notify = () => {
+        notificationSound.play();
     }
 
-    
-
-  render(){
-    var elements= [];
-    var visible = true;
-    var allElements = this.state.allNotifications;
-    
-    for(var i=0; i<allElements.length; i++){
-       elements.push(<div value = {allElements[i]} />)
+    const returnRightLink = (action) => {
+        switch (action) {
+            case "login":
+                return "/dashboard/user_log";
+            case "password_change":
+                return "/dashboard/user_log";
+            case "open_office":
+                return "/dashboard/user_log";
+            default:
+                return "/dashboard/home";
+        }
     }
-    console.log(elements.length);
-      
+
+    const returnRightPage = (action) => {
+        switch (action) {
+            case "login":
+                return "6";
+            case "password_change":
+                return "6";
+            case "open_office":
+                return "6";
+            default:
+                return "1";
+        }
+    }
+
     return (
         <div>
-            <div >
-                <Img src = {require('../img/bell_icon.png')} width = '20px' height = '20px' />
-                <div style= {{display: visible ? "visible" : "hidden"} }> 
-                    {elements}
+            <Dropdown disabled={notifications.length === 0} overlay={
+                <Menu>
+                    {notifications.map((notif, index) =>
+                        <Menu.Item key={index} onClick={() => {
+                            props.setSelected(returnRightPage(notif.payload.action));
+                            if (sel[index] === 0) {
+                                // vec jednom selektovana
+                                return;
+                            }
+                            setCount(count - 1);
+                            sel[index] = 0;
+                            setSel(sel);
+                        }}>
+                            <Link to={returnRightLink(notif.payload.action)}>
+                                <div style={sel[index] === 0 ? { color: 'grey' } : { color: 'red' }}>
+                                    <img alt="Notification type" height="20px" src={`/${notif.type}.png`} />
+                                    {" " + notif.payload.description}
+                                </div>
+                            </Link>
+                        </Menu.Item>
+                    )}
+                </Menu>
+            }>
+                <div>
+                    <img alt="Bell" style={{ marginTop: '15px' }} height="40px" className="ant-menu-item" src={count === 0 ? "/bell.png" : "/bell2.png"} />
+                    <div style={{ fontWeight: 'bold', color: count === 0 ? 'white' : 'red', marginTop: '-68px', marginLeft: '53px' }}>
+                        {count}
+                    </div>
                 </div>
-            </div>
-            
+            </Dropdown>
         </div>
-        
-      )
-  }
-  
+    )
 }
 
 export default Notifications;
